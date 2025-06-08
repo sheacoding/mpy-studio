4cb945045d6d5f6d7814f9fbb3b0b84218f3d3cd
李明 <flashpf@qq.com>
2025-06-09 01:35:40 +0800

    fixed bugs.

--- typings/mpbit/educore/__init__.pyi
+++ typings/mpbit/educore/__init__.pyi
@@ -6,10 +6,16 @@ from educore.rgb import rgb
 from educore.button import button
 # 蜂鸣器
 from educore.speaker import speaker
+# 声音传感器
+from educore.sound import sound
+# 光线传感器
+from educore.light import light
 
 __all__ = [
     "pin",
     "rgb",
     "button",
     "speaker",
+    "sound",
+    "light",
 ]
